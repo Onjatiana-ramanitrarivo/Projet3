7016ef5c33b6fad3971d8e1e226c16e1b1d52ef6
onjatiana ramanitrarivo <onjatiana.ramanitrarivo@gmail.com>
2023-12-05 15:16:57 +0300

    MàJ structure projet

--- src/main/java/mg/inclusiv/cdan/repository/ProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package mg.inclusiv.cdan.repository;
-
-import mg.inclusiv.cdan.entity.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProduitRepository extends JpaRepository<Produit , Long> {
-}
